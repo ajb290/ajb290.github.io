1eeab57ecf0dadddfbcb85841104fbe2bca70256
ajb290 <burch40342@gmail.com>
2025-01-26 17:16:34 +0000

    Update script.js
    Created function to display the value of the Rate slider

--- Simple Interest Calculator/script.js
+++ Simple Interest Calculator/script.js
@@ -3,4 +3,9 @@ function compute()
     p = document.getElementById("principal").value;
     
 }
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
+}
         
