d7c22e094f10586a2d82fb831693ecac2230a724
ajb290 <burch40342@gmail.com>
2025-01-26 17:30:37 +0000

    Update script.js
    Created the variables withing the compute() function for the Compute Interest button functionality.

--- Simple Interest Calculator/script.js
+++ Simple Interest Calculator/script.js
@@ -1,6 +1,14 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * num_years * rate / 100;
+    var amount = parseInt(principal) + parseFloat(interest);
+    var result = document.getElementById("result");
+
     
 }
 
